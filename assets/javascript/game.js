d195a3b3682a880b45230938ab2cb1961111093f
Chris Mlinac <chrismlinac@gmail.com>
2019-09-10 18:55:44 -0500

    pivot to cat collector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var wins = 0;
 var losses = 0;
 var score;
 var goal;
-var crystals;
+var cats;
 
 // start or reset the game
 function reset() {
@@ -10,12 +10,12 @@ function reset() {
     score = 0;
     goal = Math.floor(Math.random() * 101) + 19;
 
-    // make an array of crystal values, random numbers between 1 and 12
-    crystals = []
+    // make an array of cat values, random numbers between 1 and 12
+    cats = []
     for (var i = 0; i < 4; i++) {
-        crystals.push(Math.floor(Math.random() * 12) + 1);
-        // log crystal values to make the TA's job easier.
-        console.log("Crystal " + (i+1) + ": " + crystals[i]);
+        cats.push(Math.floor(Math.random() * 12) + 1);
+        // log cat values to make the TA's job easier.
+        console.log("Cat " + (i+1) + ": " + cats[i]);
     }
     console.log("-------------------------");
     // write the score and goal on the board
@@ -25,11 +25,11 @@ function reset() {
 
 reset();
 
-// run this function whenever the user clicks on a crystal
-$(".crystal").on("click", function() {
-    // get the value of the crystal and add it to the score
-    var crystalIndex = $(this).val();
-    score += crystals[crystalIndex];
+// run this function whenever the user clicks on a cat
+$(".cat").on("click", function() {
+    // get the value of the cat and add it to the score
+    var catIndex = $(this).val();
+    score += cats[catIndex];
 
     // hide win or loss from last game
     $("#result").text("")
